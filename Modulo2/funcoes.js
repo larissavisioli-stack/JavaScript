b5a20d01271f5546c36a7a8f85ca52caa3ce3154
Lari_Visioli <larissa.visioli@edu.senai.br>
2026-08-21 08:33:54 -0300

    funcoes

---
+++ Modulo2/funcoes.js
@@ -0,0 +1,16 @@
+function quandoClicarNoBotao(){
+    console.log("Obrigado por comprar em nossa loja");
+}
+
+//invocacao da função
+quandoClicarNoBotao();
+
+function exibirmensagem(){
+    console.log("Bebam água");
+}
+exibirmensagem();
+
+//Mais simples de tudo (parametros)
+//---------------------------------------
+
+
