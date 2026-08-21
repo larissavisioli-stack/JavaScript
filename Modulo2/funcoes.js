b7b139d688bbb44c67aa3c74d39090a98d0e18fb
Lari_Visioli <larissa.visioli@edu.senai.br>
2026-08-21 08:39:51 -0300

    mudando

--- Modulo2/funcoes.js
+++ Modulo2/funcoes.js
@@ -13,4 +13,15 @@ exibirmensagem();
 //Mais simples de tudo (parametros)
 //---------------------------------------
 
+function somar(){
+    const resultado = 8 + 3;
+    console.log(resultado);
+}
+somar();
 
+//----- Reutilizável
+
+function somar(numero1, numero2){
+    console.log (numero1 + numero2);
+}
+somar(8, 3);
